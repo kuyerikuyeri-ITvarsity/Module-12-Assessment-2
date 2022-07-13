9460801a423db41f275f1f9249cb1cbcfe285a0a
Kuyerik <105495543+Kuyerik@users.noreply.github.com>
2022-07-13 14:08:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
-let apiKey = checkApiKey();
+//let apiKey = checkApiKey();
 
- function checkApiKey(){
-     if(!localStorage.getItem("apiKey")){
-         window.open("enter-api-key.html", "_self");
-     }
-     return localStorage.getItem("apiKey");
- }
+ //function checkApiKey(){
+     //if(!localStorage.getItem("apiKey")){
+         //window.open("enter-api-key.html", "_self");
+     //}
+     //return localStorage.getItem("apiKey");
+ //}
